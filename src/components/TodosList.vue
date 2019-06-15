5e284f770af97de0a50897615212d4217666cb07
Siderlan Santos <siderlan.santos@gmail.com>
2019-06-15 11:17:54 -0300

    Add TodosList component

---
+++ src/components/TodosList.vue
@@ -0,0 +1,15 @@
+<template>
+    <ul class="todos-list">
+        <li v-for="todo in todos" :key="todo.id">
+            {{ todo.title }}
+        </li> 
+    </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    todos: Array
+  }
+};
+</script>

d74d461eee1f4218ba6159b5b47f0213af07cb83
Siderlan Santos <siderlan.santos@gmail.com>
2019-06-15 11:17:35 -0300

    Install vue-router plugin

---
+++ src/views/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Bootcamp Todos</h1>
+    <TodosList :todos="todos"></TodosList>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import TodosList from "@/components/TodosList.vue";
+
+export default {
+  name: "home",
+  components: {
+    TodosList
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Lista 1",
+          created_by: "2"
+        },
+        {
+          id: 2,
+          title: "Lista 2",
+          created_by: "2"
+        },
+        {
+          id: 3,
+          title: "Lista 3",
+          created_by: "2"
+        }
+      ]
+    };
+  }
+};
+</script>

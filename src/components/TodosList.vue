3151bf1b4b72ae42d1b78fff5a3d689b7da62608
Siderlan Santos <siderlan.santos@gmail.com>
2019-06-15 15:14:55 -0300

    Apply vuetify layout to Todosist component

--- src/components/TodosList.vue
+++ src/components/TodosList.vue
@@ -1,15 +1,31 @@
 <template>
-    <ul class="todos-list">
-        <li v-for="todo in todos" :key="todo.id">
-            {{ todo.title }}
-        </li> 
-    </ul>
+    <v-list>
+      <template v-for="(todo, index) in todos">
+        <v-list-tile @click="onTodoClick" :key="todo.id">
+          <v-list-tile-content>
+            <v-list-tile-title>
+              {{ todo.title }}
+            </v-list-tile-title>
+          </v-list-tile-content>  
+        </v-list-tile> 
+
+        <v-divider v-if="showDivider(index)" :key="index"></v-divider>
+      </template>
+    </v-list>
 </template>
 
 <script>
 export default {
   props: {
     todos: Array
+  },
+
+  methods: {
+    onTodoClick() {},
+
+    showDivider(index) {
+      return index + 1 < this.todos.length;
+    }
   }
 };
 </script>
